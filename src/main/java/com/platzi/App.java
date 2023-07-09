e0a7a7bbee6aaab8912cd1cc6bb219a7c1e96bae
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-09 15:11:23 -0500

    first commit

---
+++ src/main/java/com/platzi/App.java
@@ -0,0 +1,17 @@
+package com.platzi;
+
+import com.platzi.util.StringUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String result = StringUtil.repeat("Hola", 3);
+
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/com/platzi/util/StringUtilTest.java
@@ -0,0 +1,10 @@
+package com.platzi.util;
+
+public class StringUtilTest {
+    
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("Hola", 3);
+
+        System.out.println(result);
+    }
+}
