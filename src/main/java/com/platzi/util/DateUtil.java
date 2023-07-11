b0c1747505fd11751f97e3452b173ddbd13c7c3e
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-11 07:24:13 -0500

    Test to know if a year is a leap year or not

---
+++ src/main/java/com/platzi/util/DateUtil.java
@@ -0,0 +1,11 @@
+package com.platzi.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+}
